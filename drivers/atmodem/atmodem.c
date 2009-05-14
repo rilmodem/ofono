511d4f2ce5f34888f62d9416fd13f5b455e2e976
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2009-05-15 00:15:03 +0200

    drivers/atmodem implementation for CallBarring.

--- drivers/atmodem/atmodem.c
+++ drivers/atmodem/atmodem.c
@@ -109,6 +109,7 @@ static void manager_free(gpointer user)
 		at_voicecall_exit(at->modem);
 		at_ussd_exit(at->modem);
 		at_call_meter_exit(at->modem);
+		at_call_barring_exit(at->modem);
 		ofono_modem_unregister(at->modem);
 
 		at_destroy(at);
@@ -342,6 +343,7 @@ static void create_cb(GIOChannel *io, gboolean success, gpointer user)
 	at_network_registration_init(at->modem);
 	at_voicecall_init(at->modem);
 	at_call_meter_init(at->modem);
+	at_call_barring_init(at->modem);
 
 	at->io = io;
 	at->driver = g_strdup(driver);
---
+++ drivers/atmodem/call-barring.c
@@ -0,0 +1,162 @@
+/*
+ *
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2008-2009  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#define _GNU_SOURCE
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <glib.h>
+
+#include <ofono/log.h>
+#include "driver.h"
+
+#include "gatchat.h"
+#include "gatresult.h"
+
+#include "at.h"
+
+static const char *clck_prefix[] = { "+CLCK:", NULL };
+static const char *none_prefix[] = { NULL };
+
+static void clck_query_cb(gboolean ok, GAtResult *result, gpointer user_data)
+{
+	struct cb_data *cbd = user_data;
+	ofono_call_barring_cb_t cb = cbd->cb;
+	struct ofono_error error;
+	GAtResultIter iter;
+	int status_mask, status, class, line;
+
+	dump_response("clck_query_cb", ok, result);
+	decode_at_error(&error, g_at_result_final_response(result));
+
+	status_mask = 0;
+	line = 0;
+	g_at_result_iter_init(&iter, result);
+	while (g_at_result_iter_next(&iter, "+CLCK:")) {
+		line ++;
+
+		if (!g_at_result_iter_next_number(&iter, &status))
+			continue;
+
+		if (!g_at_result_iter_next_number(&iter, &class)) {
+			if (line > 1)
+				continue;
+			else
+				class = 7;
+		}
+
+		if (status)
+			status_mask |= class;
+		else
+			status_mask &= ~class;
+	}
+
+	cb(&error, status_mask, cbd->data);
+}
+
+static void at_call_barring_query(struct ofono_modem *modem, const char *lock,
+			ofono_call_barring_cb_t cb, void *data)
+{
+	struct at_data *at = ofono_modem_userdata(modem);
+	struct cb_data *cbd = cb_data_new(modem, cb, data);
+	char buf[64];
+
+	if (!cbd || strlen(lock) != 2)
+		goto error;
+
+	sprintf(buf, "AT+CLCK=\"%s\",2", lock);
+
+	if (g_at_chat_send(at->parser, buf, clck_prefix,
+				clck_query_cb, cbd, g_free) > 0)
+		return;
+
+error:
+	if (cbd)
+		g_free(cbd);
+
+	{
+		DECLARE_FAILURE(error);
+		cb(&error, 0, data);
+	}
+}
+
+static void clck_set_cb(gboolean ok, GAtResult *result, gpointer user_data)
+{
+	struct cb_data *cbd = user_data;
+	ofono_generic_cb_t cb = cbd->cb;
+	struct ofono_error error;
+
+	dump_response("clck_set_cb", ok, result);
+	decode_at_error(&error, g_at_result_final_response(result));
+	cb(&error, cbd->data);
+}
+
+static void at_call_barring_set(struct ofono_modem *modem, const char *lock,
+			int enable, const char *passwd, int cls,
+			ofono_generic_cb_t cb, void *data)
+{
+	struct at_data *at = ofono_modem_userdata(modem);
+	struct cb_data *cbd = cb_data_new(modem, cb, data);
+	char buf[64];
+	int len;
+
+	if (!cbd || strlen(lock) != 2 || (cls && !passwd))
+		goto error;
+
+	len = snprintf(buf, sizeof(buf), "AT+CLCK=\"%s\",%i", lock, !!enable);
+	if (passwd) {
+		len += snprintf(buf + len, sizeof(buf) - len,
+				",\"%s\"", passwd);
+		/* Assume cls == 0 means use defaults */
+		if (cls)
+			len += snprintf(buf + len, sizeof(buf) - len,
+					",%i", cls);
+	}
+
+	if (g_at_chat_send(at->parser, buf, none_prefix,
+				clck_set_cb, cbd, g_free) > 0)
+		return;
+
+error:
+	if (cbd)
+		g_free(cbd);
+
+	{
+		DECLARE_FAILURE(error);
+		cb(&error, data);
+	}
+}
+
+static struct ofono_call_barring_ops ops = {
+	.set	= at_call_barring_set,
+	.query	= at_call_barring_query,
+};
+
+void at_call_barring_init(struct ofono_modem *modem)
+{
+	ofono_call_barring_register(modem, &ops);
+}
+
+void at_call_barring_exit(struct ofono_modem *modem)
+{
+	ofono_call_barring_unregister(modem);
+}
