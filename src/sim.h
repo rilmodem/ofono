1ec714f49e4ce2c16a81895d0e8e843351d42f48
Denis Kenzior <denkenz@gmail.com>
2009-06-22 14:46:31 -0500

    Make signature match properly

--- src/sim.h
+++ src/sim.h
@@ -29,5 +29,5 @@ int ofono_spn_update_notify_register(struct ofono_modem *modem,
 int ofono_spn_update_notify_unregister(struct ofono_modem *modem,
 		update_spn_cb cb);
 
-int ofono_operator_in_spdi(struct ofono_modem *modem,
+gboolean ofono_operator_in_spdi(struct ofono_modem *modem,
 				const struct ofono_network_operator *op);
