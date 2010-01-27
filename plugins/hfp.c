abf86ae5fa8789e6f1a9934ac9ab5c3cf1bc1d4e
Gustavo F. Padovan <padovan@profusion.mobi>
2010-01-27 18:28:05 -0200

    Add copyright note to the HFP plugin

--- plugins/hfp.c
+++ plugins/hfp.c
@@ -2,6 +2,7 @@
  *  oFono - Open Source Telephony
  *
  *  Copyright (C) 2008-2010  Intel Corporation. All rights reserved.
+ *  Copyright (C) 2010  Gustavo F. Padovan <padovan@profusion.mobi>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
