8e91f477f94d055c2e2a4e7db440bafc3c79bd8f
Yang Gu <yang.gu@intel.com>
2010-04-02 14:20:27 +0800

    Add parser for card atr objects

--- src/stkutil.c
+++ src/stkutil.c
@@ -771,6 +771,24 @@ static gboolean parse_dataobj_card_reader_status(
 	return parse_dataobj_common_byte(iter, byte);
 }
 
+/* Defined in TS 102.223 Section 8.34 */
+static gboolean parse_dataobj_card_atr(
+		struct comprehension_tlv_iter *iter, void *user)
+{
+	struct stk_card_atr *ca = user;
+	const unsigned char *data;
+	unsigned int len = comprehension_tlv_iter_get_length(iter);
+
+	if ((len < 1) || (len > sizeof(ca->atr)))
+		return FALSE;
+
+	data = comprehension_tlv_iter_get_data(iter);
+	ca->len = len;
+	memcpy(ca->atr, data, len);
+
+	return TRUE;
+}
+
 /* Defined in 102.223 Section 8.43 */
 static gboolean parse_dataobj_imm_resp(struct comprehension_tlv_iter *iter,
 					void *user)
@@ -877,6 +895,8 @@ static dataobj_handler handler_for_type(enum stk_data_object_type type)
 		return parse_dataobj_item_icon_id_list;
 	case STK_DATA_OBJECT_TYPE_CARD_READER_STATUS:
 		return parse_dataobj_card_reader_status;
+	case STK_DATA_OBJECT_TYPE_CARD_ATR:
+		return parse_dataobj_card_atr;
 	case STK_DATA_OBJECT_TYPE_IMMEDIATE_RESPONSE:
 		return parse_dataobj_imm_resp;
 	case STK_DATA_OBJECT_TYPE_TEXT_ATTRIBUTE:
--- src/stkutil.h
+++ src/stkutil.h
@@ -460,6 +460,15 @@ struct stk_item_icon_id_list {
 };
 
 /*
+ * According to 102.223 Section 8.34 the length of CTLV is 1 byte. This means
+ * that the maximum size is 127 according to the rules of CTLVs.
+ */
+struct stk_card_atr {
+	unsigned char atr[127];
+	unsigned int len;
+};
+
+/*
  * According to 102.223 Section 8.72 the length of text attribute CTLV is 1
  * byte.  This means that the maximum size is 127 according to the rules
  * of CTLVs.  Empty attribute options will have len of 0.
