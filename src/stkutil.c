c25fba0f07a5d4f1418500716adeaf67fb79e893
Yang Gu <yang.gu@intel.com>
2010-04-06 18:06:43 +0800

    Add parser for channel data length objects

--- src/stkutil.c
+++ src/stkutil.c
@@ -1126,6 +1126,14 @@ static gboolean parse_dataobj_channel_data(struct comprehension_tlv_iter *iter,
 	return parse_dataobj_common_byte_array(iter, array);
 }
 
+/* Defined in TS 102.223 Section 8.54 */
+static gboolean parse_dataobj_channel_data_length(
+		struct comprehension_tlv_iter *iter, void *user)
+{
+	unsigned char *byte = user;
+	return parse_dataobj_common_byte(iter, byte);
+}
+
 /* Defined in TS 102.223 Section 8.72 */
 static gboolean parse_dataobj_text_attr(struct comprehension_tlv_iter *iter,
 					void *user)
@@ -1262,6 +1270,8 @@ static dataobj_handler handler_for_type(enum stk_data_object_type type)
 		return parse_dataobj_bearer_description;
 	case STK_DATA_OBJECT_TYPE_CHANNEL_DATA:
 		return parse_dataobj_channel_data;
+	case STK_DATA_OBJECT_TYPE_CHANNEL_DATA_LENGTH:
+		return parse_dataobj_channel_data_length;
 	case STK_DATA_OBJECT_TYPE_TEXT_ATTRIBUTE:
 		return parse_dataobj_text_attr;
 	case STK_DATA_OBJECT_TYPE_FRAME_ID:
