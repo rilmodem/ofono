1f5432abd63f011d99421f237ecc5eea1b8d5694
Marcel Holtmann <marcel@holtmann.org>
2010-06-04 12:13:27 -0700

    Use AT_OUWCTI instead of AT_OWCTI as expected
    The Option firmware is so funny. It has a typo in the AT command :)

--- drivers/atmodem/network-registration.c
+++ drivers/atmodem/network-registration.c
@@ -856,7 +856,7 @@ static void at_creg_set_cb(gboolean ok, GAtResult *result, gpointer user_data)
 	case OFONO_VENDOR_OPTION_HSO:
 		g_at_chat_send(nd->chat, "AT_OSSYS=1", none_prefix,
 				NULL, NULL, NULL);
-		g_at_chat_send(nd->chat, "AT_OWCTI=1", none_prefix,
+		g_at_chat_send(nd->chat, "AT_OUWCTI=1", none_prefix,
 				NULL, NULL, NULL);
 		g_at_chat_send(nd->chat, "AT_OCTI=1", none_prefix,
 				NULL, NULL, NULL);
