a4aeedc3f095cdc2d9d7938783b91c38433f96d5
Marcel Holtmann <marcel@holtmann.org>
2010-09-18 22:18:51 +0900

    ifxmodem: Add skeleton for Infineon driver plugin

---
+++ drivers/ifxmodem/ifxmodem.c
@@ -0,0 +1,46 @@
+/*
+ *
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2008-2010  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib.h>
+#include <gatchat.h>
+
+#define OFONO_API_SUBJECT_TO_CHANGE
+#include <ofono/plugin.h>
+#include <ofono/types.h>
+
+#include "ifxmodem.h"
+
+static int ifxmodem_init(void)
+{
+	return 0;
+}
+
+static void ifxmodem_exit(void)
+{
+}
+
+OFONO_PLUGIN_DEFINE(ifxmodem, "Infineon modem driver", VERSION,
+			OFONO_PLUGIN_PRIORITY_DEFAULT,
+			ifxmodem_init, ifxmodem_exit)
