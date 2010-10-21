cca078202918e4c10890919b85928917fac2483a
Marcel Holtmann <marcel@holtmann.org>
2010-10-21 20:32:18 +0200

    ifx: Use Infineon specific phonebook vendor quirk

--- plugins/ifx.c
+++ plugins/ifx.c
@@ -654,7 +654,8 @@ static void ifx_post_sim(struct ofono_modem *modem)
 	DBG("%p", modem);
 
 	ofono_stk_create(modem, 0, "ifxmodem", data->dlcs[AUX_DLC]);
-	ofono_phonebook_create(modem, 0, "atmodem", data->dlcs[AUX_DLC]);
+	ofono_phonebook_create(modem, OFONO_VENDOR_IFX,
+					"atmodem", data->dlcs[AUX_DLC]);
 }
 
 static void ifx_post_online(struct ofono_modem *modem)
