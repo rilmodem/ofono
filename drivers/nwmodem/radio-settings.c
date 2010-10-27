64f327e2d0990904d7b8ef5f506720101921c359
Marcel Holtmann <marcel@holtmann.org>
2010-10-27 11:05:33 +0200

    nwmodem: Fix handling of radio access mode enum

--- drivers/nwmodem/radio-settings.c
+++ drivers/nwmodem/radio-settings.c
@@ -126,7 +126,7 @@ static void nw_set_rat_mode(struct ofono_radio_settings *rs,
 	struct radio_settings_data *rsd = ofono_radio_settings_get_data(rs);
 	struct cb_data *cbd = cb_data_new(cb, data);
 	char buf[20];
-	int value;
+	int value = 0;
 
 	switch (mode) {
 	case OFONO_RADIO_ACCESS_MODE_ANY:
@@ -138,19 +138,19 @@ static void nw_set_rat_mode(struct ofono_radio_settings *rs,
 	case OFONO_RADIO_ACCESS_MODE_UMTS:
 		value = 2;
 		break;
-	default:
-		CALLBACK_WITH_FAILURE(cb, data);
-		g_free(cbd);
-		return;
+	case OFONO_RADIO_ACCESS_MODE_LTE:
+		goto error;
 	}
 
 	snprintf(buf, sizeof(buf), "AT$NWRAT=%u,2", value);
 
 	if (g_at_chat_send(rsd->chat, buf, none_prefix,
-					nwrat_modify_cb, cbd, g_free) == 0) {
-		CALLBACK_WITH_FAILURE(cb, data);
-		g_free(cbd);
-	}
+					nwrat_modify_cb, cbd, g_free) > 0)
+		return;
+
+error:
+	CALLBACK_WITH_FAILURE(cb, data);
+	g_free(cbd);
 }
 
 static void nwrat_support_cb(gboolean ok, GAtResult *result, gpointer user_data)
