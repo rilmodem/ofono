939d66db51f1ab9cc0c2812a4072c5a66b998487
Denis Kenzior <denkenz@gmail.com>
2010-12-16 18:39:15 -0600

    examples: Add network name handling to history

--- examples/history.c
+++ examples/history.c
@@ -68,6 +68,9 @@ static void example_history_call_ended(struct ofono_history_context *context,
 	else
 		ofono_debug("From: %s", from);
 
+	if (call->cnap_validity == 0)
+		ofono_debug("Name from Network: %s\n", call->name);
+
 	strftime(buf, 127, "%Y-%m-%dT%H:%M:%S%z", localtime(&start));
 	buf[127] = '\0';
 	ofono_debug("StartTime: %s", buf);
@@ -96,6 +99,10 @@ static void example_history_call_missed(struct ofono_history_context *context,
 		from = phone_number_to_string(&call->phone_number);
 
 	ofono_debug("From: %s", from);
+
+	if (call->cnap_validity == 0)
+		ofono_debug("Name from Network: %s\n", call->name);
+
 	strftime(buf, 127, "%Y-%m-%dT%H:%M:%S%z", localtime(&when));
 	buf[127] = '\0';
 	ofono_debug("When: %s", buf);
