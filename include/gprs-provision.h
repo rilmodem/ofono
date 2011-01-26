712d97802194ae41b709f9c57914be572b245c5c
Denis Kenzior <denkenz@gmail.com>
2011-01-26 14:48:37 -0600

    include: Refactor gprs-provision API

--- include/gprs-provision.h
+++ include/gprs-provision.h
@@ -42,7 +42,7 @@ struct ofono_gprs_provision_data {
 struct ofono_gprs_provision_driver {
 	const char *name;
 	int priority;
-	void (*get_settings)(const char *mcc, const char *mnc,
+	int (*get_settings)(const char *mcc, const char *mnc,
 				struct ofono_gprs_provision_data **settings,
 				int *count);
 };
