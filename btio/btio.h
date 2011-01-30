3970e5c06b8049ad10ff2b6f92ef5d2dd62e39b6
Marcel Holtmann <marcel@holtmann.org>
2011-01-30 10:56:37 +0100

    btio: Fix email address in copyright header

--- btio/btio.h
+++ btio/btio.h
@@ -2,7 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
- *  Copyright (C) 2009-2010  Marcel Holtmann <marcel at holtmann.org>
+ *  Copyright (C) 2009-2010  Marcel Holtmann <marcel@holtmann.org>
  *  Copyright (C) 2009-2010  Nokia Corporation
  *
  *
