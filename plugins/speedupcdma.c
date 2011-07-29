98d448e7c4c97fc1b0f770e2467edb6515408b49
Marcel Holtmann <marcel@holtmann.org>
2011-07-29 15:25:24 +0200

    speedupcdma: Fix stale control channel after hot-unplug

--- plugins/speedupcdma.c
+++ plugins/speedupcdma.c
@@ -72,6 +72,9 @@ static void speedupcdma_remove(struct ofono_modem *modem)
 
 	ofono_modem_set_data(modem, NULL);
 
+	/* Cleanup after hot-unplug */
+	g_at_chat_unref(data->aux);
+
 	g_free(data);
 }
 
