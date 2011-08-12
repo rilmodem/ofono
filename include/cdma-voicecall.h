949f0ad44756cebf5dc8e5cb54ea23fb4bb23684
Caiwen Zhang <caiwen.zhang@windriver.com>
2011-08-12 17:19:46 +0800

    include: Add send_flash to cdma voicecall driver API

--- include/cdma-voicecall.h
+++ include/cdma-voicecall.h
@@ -58,6 +58,9 @@ struct ofono_cdma_voicecall_driver {
 
 	void (*answer)(struct ofono_cdma_voicecall *vc,
 			ofono_cdma_voicecall_cb_t cb, void *data);
+
+	void (*send_flash)(struct ofono_cdma_voicecall *vc, const char *string,
+			ofono_cdma_voicecall_cb_t cb, void *data);
 };
 
 void ofono_cdma_voicecall_disconnected(struct ofono_cdma_voicecall *vc,
