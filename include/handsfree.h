ac4c9d72371687290123d6bf5b02230a9d407dcf
Marcel Holtmann <marcel@holtmann.org>
2011-10-15 12:21:12 -0700

    core: Some more copyright fixes

--- include/handsfree.h
+++ include/handsfree.h
@@ -3,7 +3,7 @@
  *  oFono - Open Source Telephony
  *
  *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
- *  Copyright (C) 2011 BMW Car IT GmbH. All rights reserved.
+ *  Copyright (C) 2011  BMW Car IT GmbH. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
--- src/idmap.c
+++ src/idmap.c
@@ -3,7 +3,7 @@
  *  oFono - Open Source Telephony
  *
  *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
- *  Copyright (C) 2004 Red Hat, Inc. All Rights Reserved.
+ *  Copyright (C) 2004  Red Hat, Inc. All Rights Reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
