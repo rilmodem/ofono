c867a281849acc3ff3c041f9e1ccfbd3e36d9a69
Denis Kenzior <denkenz@gmail.com>
2012-10-18 12:46:24 -0500

    stktest: Add GetInput test sequence 1.7

--- tools/stktest.c
+++ tools/stktest.c
@@ -2307,6 +2307,25 @@ static DBusMessage *test_get_input_16(DBusMessage *msg,
 	return stktest_error_go_back(msg);
 }
 
+static DBusMessage *test_get_input_17(DBusMessage *msg,
+					const char *alpha,
+					unsigned char icon_id,
+					const char *def_input,
+					unsigned char min, unsigned char max,
+					gboolean hide_typing)
+{
+	DBusMessage *reply;
+
+	STKTEST_AGENT_ASSERT(g_str_equal(alpha, "&lt;ABORT&gt;"));
+	STKTEST_AGENT_ASSERT(icon_id == 0);
+	STKTEST_AGENT_ASSERT(g_str_equal(def_input, ""));
+	STKTEST_AGENT_ASSERT(min == 0);
+	STKTEST_AGENT_ASSERT(max == 8);
+	STKTEST_AGENT_ASSERT(hide_typing == FALSE);
+
+	return stktest_error_end_session(msg);
+}
+
 static void power_down_reply(DBusPendingCall *call, void *user_data)
 {
 	__stktest_test_next();
@@ -2786,6 +2805,12 @@ static void __stktest_test_init(void)
 				get_input_response_161,
 				sizeof(get_input_response_161),
 				test_get_input_16,
+				expect_response_and_finish);
+	stktest_add_test("Get Input 1.7", "RequestDigits",
+				get_input_171, sizeof(get_input_171),
+				get_input_response_171,
+				sizeof(get_input_response_171),
+				test_get_input_17,
 				expect_response_and_finish);
 }
 
