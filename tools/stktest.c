cb70584ce64e021e9a7716507abcadd1652ebc98
Denis Kenzior <denkenz@gmail.com>
2012-10-18 12:01:14 -0500

    stktest: Add GetInput test sequence 1.3

--- tools/stktest.c
+++ tools/stktest.c
@@ -2214,6 +2214,30 @@ static DBusMessage *test_get_input_12(DBusMessage *msg,
 	return reply;
 }
 
+static DBusMessage *test_get_input_13(DBusMessage *msg,
+					const char *alpha,
+					unsigned char icon_id,
+					const char *def_input,
+					unsigned char min, unsigned char max,
+					gboolean hide_typing)
+{
+	DBusMessage *reply;
+	const char *ret = "AbCdE";
+
+	STKTEST_AGENT_ASSERT(g_str_equal(alpha, "Enter AbCdE"));
+	STKTEST_AGENT_ASSERT(icon_id == 0);
+	STKTEST_AGENT_ASSERT(g_str_equal(def_input, ""));
+	STKTEST_AGENT_ASSERT(min == 5);
+	STKTEST_AGENT_ASSERT(max == 5);
+	STKTEST_AGENT_ASSERT(hide_typing == FALSE);
+
+	reply = dbus_message_new_method_return(msg);
+	dbus_message_append_args(reply, DBUS_TYPE_STRING, &ret,
+					DBUS_TYPE_INVALID);
+
+	return reply;
+}
+
 static void power_down_reply(DBusPendingCall *call, void *user_data)
 {
 	__stktest_test_next();
@@ -2669,6 +2693,12 @@ static void __stktest_test_init(void)
 				get_input_response_121,
 				sizeof(get_input_response_121),
 				test_get_input_12,
+				expect_response_and_finish);
+	stktest_add_test("Get Input 1.3", "RequestInput",
+				get_input_131, sizeof(get_input_131),
+				get_input_response_131,
+				sizeof(get_input_response_131),
+				test_get_input_13,
 				expect_response_and_finish);
 }
 
