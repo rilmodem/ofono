b728a029819587a4ffca52412b70c086f9728eaf
Cedric Jehasse <cedric.jehasse@softathome.com>
2012-11-15 09:35:51 +0100

    atmodem: add PIN retry count for Alcatel modems

--- drivers/atmodem/sim.c
+++ drivers/atmodem/sim.c
@@ -66,6 +66,7 @@ static const char *cpinr_prefixes[] = { "+CPINR:", "+CPINRE:", NULL };
 static const char *epin_prefix[] = { "*EPIN:", NULL };
 static const char *spic_prefix[] = { "+SPIC:", NULL };
 static const char *pct_prefix[] = { "#PCT:", NULL };
+static const char *pnnm_prefix[] = { "+PNNM:", NULL };
 static const char *none_prefix[] = { NULL };
 
 static void at_crsm_info_cb(gboolean ok, GAtResult *result, gpointer user_data)
@@ -899,6 +900,43 @@ error:
 	CALLBACK_WITH_FAILURE(cb, NULL, cbd->data);
 }
 
+static void at_pnnm_cb(gboolean ok, GAtResult *result, gpointer user_data)
+{
+	struct cb_data *cbd = user_data;
+	ofono_sim_pin_retries_cb_t cb = cbd->cb;
+	const char *final = g_at_result_final_response(result);
+	GAtResultIter iter;
+	struct ofono_error error;
+	int retries[OFONO_SIM_PASSWORD_INVALID];
+	size_t i;
+	static enum ofono_sim_password_type password_types[] = {
+		OFONO_SIM_PASSWORD_SIM_PIN,
+		OFONO_SIM_PASSWORD_SIM_PUK,
+	};
+
+	decode_at_error(&error, final);
+
+	if (!ok) {
+		cb(&error, NULL, cbd->data);
+		return;
+	}
+
+	g_at_result_iter_init(&iter, result);
+
+	if (!g_at_result_iter_next(&iter, "+PNNM:"))
+		goto error;
+
+	BUILD_PIN_RETRIES_ARRAY(password_types, ARRAY_SIZE(password_types),
+				retries);
+
+	cb(&error, retries, cbd->data);
+
+	return;
+
+error:
+	CALLBACK_WITH_FAILURE(cb, NULL, cbd->data);
+}
+
 static void at_pin_retries_query(struct ofono_sim *sim,
 					ofono_sim_pin_retries_cb_t cb,
 					void *data)
@@ -953,6 +991,11 @@ static void at_pin_retries_query(struct ofono_sim *sim,
 	case OFONO_VENDOR_TELIT:
 		if (g_at_chat_send(sd->chat, "AT#PCT", pct_prefix,
 					at_pct_cb, cbd, g_free) > 0)
+			return;
+		break;
+	case OFONO_VENDOR_ALCATEL:
+		if (g_at_chat_send(sd->chat, "AT+PNNM?", pnnm_prefix,
+					at_pnnm_cb, cbd, g_free) > 0)
 			return;
 		break;
 	default:
