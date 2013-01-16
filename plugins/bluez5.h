dce0c634d05f967c797ed4ba52599b6e23feda66
Claudio Takahasi <claudio.takahasi@openbossa.org>
2013-01-16 10:31:30 -0300

    bluetooth: Add profile register / unregister
    Adds utility API to handle BlueZ 5 RegisterProfile and UnregisterProfile
    methods.

---
+++ plugins/bluez5.h
@@ -0,0 +1,29 @@
+/*
+ *
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2013  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#define	BLUEZ_SERVICE "org.bluez"
+
+#define HFP_HS_UUID	"0000111e-0000-1000-8000-00805f9b34fb"
+
+int bluetooth_register_profile(DBusConnection *conn, const char *uuid,
+					const char *name, const char *object);
+
+void bluetooth_unregister_profile(DBusConnection *conn, const char *object);
