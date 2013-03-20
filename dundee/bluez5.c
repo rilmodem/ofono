5c97aa7a1c9d4e69369a6175e90a4a0b6bafdbb3
Paulo Borges <paulo.borges@openbossa.org>
2013-03-20 19:26:31 -0300

    dundee: Start BlueZ 5 support
    This patch adds the BlueZ 5 support file for dundee.

---
+++ dundee/bluez5.c
@@ -0,0 +1,37 @@
+/*
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2013  Instituto Nokia de Tecnologia - INdT
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "dundee.h"
+
+int __dundee_bluetooth_init(void)
+{
+	DBG("");
+
+	return 0;
+}
+
+void __dundee_bluetooth_cleanup(void)
+{
+	DBG("");
+}
