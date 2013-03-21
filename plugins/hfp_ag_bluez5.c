2864624c56dd79eaf8a7122a3dbde1b4c85d8657
Denis Kenzior <denkenz@gmail.com>
2013-03-21 10:19:00 -0500

    hfp_ag_bluez5: Fix sanity check
    We should be checking that modems != NULL, not modems->data.  This is
    because the list can be empty and we would crash.

--- plugins/hfp_ag_bluez5.c
+++ plugins/hfp_ag_bluez5.c
@@ -84,14 +84,14 @@ static DBusMessage *profile_new_connection(DBusConnection *conn,
 	DBG("%s", device);
 
 	/* Pick the first voicecall capable modem */
-	modem = modems->data;
-	if (modem == NULL) {
+	if (modems == NULL) {
 		close(fd);
 		return g_dbus_create_error(msg, BLUEZ_ERROR_INTERFACE
 						".Rejected",
 						"No voice call capable modem");
 	}
 
+	modem = modems->data;
 	DBG("Picked modem %p for emulator", modem);
 
 	em = ofono_emulator_create(modem, OFONO_EMULATOR_TYPE_HFP);
