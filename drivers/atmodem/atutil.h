0026ae3fb71925fdd90739b8447ad3ae18d111fe
Denis Kenzior <denkenz@gmail.com>
2013-09-10 10:57:42 -0500

    atmodem: Update parse_clcc utility function

--- drivers/atmodem/atutil.h
+++ drivers/atmodem/atutil.h
@@ -55,7 +55,7 @@ gint at_util_call_compare_by_status(gconstpointer a, gconstpointer b);
 gint at_util_call_compare_by_phone_number(gconstpointer a, gconstpointer b);
 gint at_util_call_compare_by_id(gconstpointer a, gconstpointer b);
 gint at_util_call_compare(gconstpointer a, gconstpointer b);
-GSList *at_util_parse_clcc(GAtResult *result);
+GSList *at_util_parse_clcc(GAtResult *result, unsigned int *mpty_ids);
 gboolean at_util_parse_reg(GAtResult *result, const char *prefix,
 				int *mode, int *status,
 				int *lac, int *ci, int *tech,
