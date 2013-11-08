130da8733346c67d09abd9b83c502aeaaa974f77
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2013-11-08 14:37:12 +0100

    src: Auxiliar functions for MNC length plugins

---
+++ src/sim-mnclength.c
@@ -0,0 +1,71 @@
+/*
+ *
+ *  oFono - Open Source Telephony
+ *
+ *  Copyright (C) 2011  Nokia Corporation and/or its subsidiary(-ies).
+ *  Copyright (C) 2013  Canonical Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+#include <glib.h>
+#include "ofono.h"
+#include "sim-mnclength.h"
+
+static GSList *g_drivers = NULL;
+
+int __ofono_sim_mnclength_get_mnclength(const char *imsi)
+{
+	GSList *d;
+	int mnclen;
+
+	for (d = g_drivers; d != NULL; d = d->next) {
+		const struct ofono_sim_mnclength_driver *driver = d->data;
+
+		if (driver->get_mnclength == NULL)
+			continue;
+
+		DBG("Calling mnclength plugin '%s'", driver->name);
+
+		if ((mnclen = driver->get_mnclength(imsi)) <= 0)
+			continue;
+
+		return mnclen;
+	}
+
+	return 0;
+}
+
+int ofono_sim_mnclength_driver_register(
+			struct ofono_sim_mnclength_driver *driver)
+{
+	DBG("driver: %p name: %s", driver, driver->name);
+
+	g_drivers = g_slist_prepend(g_drivers, driver);
+	return 0;
+}
+
+void ofono_sim_mnclength_driver_unregister(
+			const struct ofono_sim_mnclength_driver *driver)
+{
+	DBG("driver: %p name: %s", driver, driver->name);
+
+	g_drivers = g_slist_remove(g_drivers, driver);
+}
