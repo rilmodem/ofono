8621d36c15d84d92c3a89cab40ec91634e59c166
Krzysztof Wilk <krzysztofx.wilk@intel.com>
2013-12-16 17:52:38 +0000

    dbus: Define Siri DBUS Interface

--- include/dbus.h
+++ include/dbus.h
@@ -59,6 +59,7 @@ extern "C" {
 #define OFONO_GNSS_INTERFACE "org.ofono.AssistedSatelliteNavigation"
 #define OFONO_GNSS_POSR_AGENT_INTERFACE "org.ofono.PositioningRequestAgent"
 #define OFONO_HANDSFREE_INTERFACE OFONO_SERVICE ".Handsfree"
+#define OFONO_SIRI_INTERFACE OFONO_SERVICE ".Siri"
 
 /* CDMA Interfaces */
 #define OFONO_CDMA_VOICECALL_MANAGER_INTERFACE "org.ofono.cdma.VoiceCallManager"
