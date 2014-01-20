165c385bdcebeb0dad5b3b7c2d0aa8f924fe2313
Denis Kenzior <denkenz@gmail.com>
2014-01-19 22:06:03 -0600

    handsfree: Add new driver method for HF indicators

--- include/handsfree.h
+++ include/handsfree.h
@@ -56,6 +56,9 @@ struct ofono_handsfree_driver {
 					ofono_handsfree_cb_t cb, void *data);
 	void (*disable_nrec)(struct ofono_handsfree *hf,
 					ofono_handsfree_cb_t cb, void *data);
+	void (*hf_indicator)(struct ofono_handsfree *hf,
+				unsigned short indicator, unsigned int value,
+				ofono_handsfree_cb_t cb, void *data);
 };
 
 void ofono_handsfree_set_ag_features(struct ofono_handsfree *hf,
