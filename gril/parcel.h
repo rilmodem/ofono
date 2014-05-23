b43327e9767340d801174f7f871857b87cc1ecde
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2014-05-23 14:30:52 +0200

    gril: Add functions for reading a string array

--- gril/parcel.h
+++ gril/parcel.h
@@ -32,6 +32,11 @@ struct parcel {
 	int malformed;
 };
 
+struct parcel_str_array {
+	int num_str;
+	char *str[];
+};
+
 void parcel_init(struct parcel *p);
 void parcel_grow(struct parcel *p, size_t size);
 void parcel_free(struct parcel *p);
@@ -42,5 +47,7 @@ char *parcel_r_string(struct parcel *p);
 int parcel_w_raw(struct parcel *p, const void *data, size_t len);
 void *parcel_r_raw(struct parcel *p,  int *len);
 size_t parcel_data_avail(struct parcel *p);
+struct parcel_str_array *parcel_r_str_array(struct parcel *p);
+void parcel_free_str_array(struct parcel_str_array *str_arr);
 
 #endif
