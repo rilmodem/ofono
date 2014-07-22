636291e2f2d69cc16b25b33313f95be06a224abc
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2014-07-22 18:32:15 +0200

    gril: Add slot member
    This member is needed by parcel parsers in some cases.

--- gril/gril.h
+++ gril/gril.h
@@ -120,6 +120,9 @@ void g_ril_set_disconnect_function(GRil *ril, GRilDisconnectFunc disconnect,
 gboolean g_ril_get_trace(GRil *ril);
 gboolean g_ril_set_trace(GRil *ril, gboolean trace);
 
+int g_ril_get_slot(GRil *ril);
+gboolean g_ril_set_slot(GRil *ril, int slot);
+
 /*!
  * If the function is not NULL, then on every read/write from the GIOChannel
  * provided to GRil the logging function will be called with the
