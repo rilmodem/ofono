b4a922061675fad7d29265a3cede59fed3f0d124
Ratchanan Srirattanamet <peathot@hotmail.com>
2015-04-14 02:19:32 +0700

    qcommsimmodem: Add Qualcomm's specific definition.

---
+++ drivers/qcommsimmodem/qcom_msim_constants.h
@@ -0,0 +1,27 @@
+/*
+ *
+ *  RIL constants for Qualcomm multi-sim modem
+ *
+ *  Copyright (C) 2015 Ratchanan Srirattanamet.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef QCOM_MSIM_CONSTANTS_H
+#define QCOM_MSIM_CONSTANTS_H
+
+#define QCOM_MSIM_RIL_REQUEST_SET_UICC_SUBSCRIPTION  115
+
+#endif /* QCOM_MSIM_CONSTANTS_H */
