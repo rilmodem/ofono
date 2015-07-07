c3af639874f208268c00fedcdee4321b8d540bc6
Marcel Holtmann <marcel@holtmann.org>
2015-07-07 11:30:20 +0200

    isiusb: Fix compiler warning with logical expression
      CC       plugins/isiusb.o
    plugins/isiusb.c: In function ‘reachable_cb’:
    plugins/isiusb.c:207:28: error: logical not is only applied to the left hand side of comparison [-Werror=logical-not-parentheses]
      if (!g_isi_msg_error(msg) < 0)
                                ^

--- plugins/isiusb.c
+++ plugins/isiusb.c
@@ -204,7 +204,7 @@ static void reachable_cb(const GIsiMessage *msg, void *data)
 	struct ofono_modem *om = data;
 	struct isi_data *isi = ofono_modem_get_data(om);
 
-	if (!g_isi_msg_error(msg) < 0)
+	if (g_isi_msg_error(msg) < 0)
 		return;
 
 	ISI_RESOURCE_DBG(msg);
