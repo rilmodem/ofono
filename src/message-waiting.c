2cae96df433668f5f8ae8cbc9cd6d5893c3fe628
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2015-07-17 08:47:15 +0200

    message-waiting: Update properly EF_MWIS SIM file

--- src/message-waiting.c
+++ src/message-waiting.c
@@ -732,6 +732,7 @@ static void mw_set_indicator(struct ofono_message_waiting *mw, int profile,
 		efmwis[i + 1] = mw->messages[i].message_count;
 
 	/* Fill in indicator state bits in byte 0 */
+	efmwis[0] = 0;
 	for (i = 0; i < 5 && i < mw->efmwis_length - 1; i++)
 		if (mw->messages[i].indication)
 			efmwis[0] |= 1 << i;
