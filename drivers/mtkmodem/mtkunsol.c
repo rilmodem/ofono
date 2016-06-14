4ed3be0e2dc561d45e4dd75a8a28eefc77e65ed7
Alfonso Sanchez-Beato <alfonso.sanchez-beato@canonical.com>
2016-06-14 09:19:16 +0200

    mtkmodem: parse newer call indication events
    Parse properly call indication events from newer modems, which add
    a forwarding address argument to the event.

--- drivers/mtkmodem/mtkunsol.c
+++ drivers/mtkmodem/mtkunsol.c
@@ -45,7 +45,8 @@ struct unsol_call_indication *g_mtk_unsol_parse_incoming_call_indication(
 	struct parcel rilp;
 	int numstr;
 	struct unsol_call_indication *unsol;
-	char *call_id, *phone, *mystery, *call_mode, *seq_num;
+	char *call_id, *phone, *address_type, *call_mode, *seq_num;
+	char *fw_address = NULL;
 	char *endp;
 
 	unsol = g_try_malloc0(sizeof(*unsol));
@@ -57,19 +58,26 @@ struct unsol_call_indication *g_mtk_unsol_parse_incoming_call_indication(
 	g_ril_init_parcel(message, &rilp);
 
 	numstr = parcel_r_int32(&rilp);
-	if (numstr != 5) {
+	if (numstr < 5) {
 		ofono_error("%s: wrong array size (%d)", __func__, numstr);
 		goto error;
 	}
 
 	call_id = parcel_r_string(&rilp);
 	phone = parcel_r_string(&rilp);
-	mystery = parcel_r_string(&rilp);
+	address_type = parcel_r_string(&rilp);
 	call_mode = parcel_r_string(&rilp);
 	seq_num = parcel_r_string(&rilp);
+	if (numstr > 5)
+		fw_address = parcel_r_string(&rilp);
 
-	g_ril_append_print_buf(gril, "{%s,%s,%s,%s,%s}", call_id, phone,
-				mystery, call_mode, seq_num);
+	g_ril_append_print_buf(gril, "{%s,%s,%s,%s,%s,%s}",
+				PRINTABLE_STR(call_id),
+				PRINTABLE_STR(phone),
+				PRINTABLE_STR(address_type),
+				PRINTABLE_STR(call_mode),
+				PRINTABLE_STR(seq_num),
+				PRINTABLE_STR(fw_address));
 
 	g_ril_print_unsol(gril, message);
 
@@ -113,18 +121,20 @@ struct unsol_call_indication *g_mtk_unsol_parse_incoming_call_indication(
 
 	g_free(call_id);
 	g_free(phone);
-	g_free(mystery);
+	g_free(address_type);
 	g_free(call_mode);
 	g_free(seq_num);
+	g_free(fw_address);
 
 	return unsol;
 
 err_conv:
 	g_free(call_id);
 	g_free(phone);
-	g_free(mystery);
+	g_free(address_type);
 	g_free(call_mode);
 	g_free(seq_num);
+	g_free(fw_address);
 
 error:
 	g_free(unsol);
